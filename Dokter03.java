9836975c37136633244e4df3743086c13aabf5ae
kartikarn <karahmaa25@gmail.com>
2025-06-05 15:27:06 +0700

    by tika

--- Dokter03.java
+++ Dokter03.java
@@ -1,3 +1,9 @@
 public class Dokter03 {
-    
+    String idDokter;
+    String nama;
+
+    public void Dokter03 (String idDokter, String nama) {
+        this.idDokter = idDokter;
+        this.nama = nama;
+    }
 }

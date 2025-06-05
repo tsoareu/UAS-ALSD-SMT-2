5cf7c8ffb41ba086c8fb885935ae0d7ee8df4e62
Alfimaksum <alpimaksum788@gmail.com>
2025-06-05 16:03:09 +0700

    by maksum

--- Pasien03.java
+++ Pasien03.java
@@ -1,4 +1,17 @@
 public class Pasien03 {
-    String pasien;
+    String Pasien;
     String NIK;
+    String Keluhan;
+
+    Public Pasien03(String Pasien, String NIK, String Keluhan) {
+        this.Pasien = Pasien;
+        this.NIK = NIK;
+        this.Keluhan = Keluhan;
+    }
+
+    public void tampilkanInformasi(){
+        System.out.println("Nama Pasien : " + Pasien);
+        System.out.println("NIK         : " + Pasien);
+        System.out.println("Keluhan     : " + Keluhan);
+    }
 }
